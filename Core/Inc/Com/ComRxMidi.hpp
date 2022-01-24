0be334fa679ce360932bce8b9880c523afb6f008
Colombel-Vincent <colombel.vincent.27@gmail.com>
2022-01-24 06:48:28 +0100

    Add com derived class (com arduino (Tx), com I2C (Tx), com Midi (Rx & Tx))

---
+++ Core/Inc/Com/ComRxMidi.hpp
@@ -0,0 +1,20 @@
+/*
+ * ComMidi.hpp
+ *
+ *  Created on: Jan 20, 2022
+ *      Author: colom
+ */
+
+#ifndef INC_COM_COMMIDI_HPP_
+#define INC_COM_COMMIDI_HPP_
+
+#include "ComManager.hpp"
+
+
+class ComMidiRx : public ComManagerRx
+{
+
+};
+
+
+#endif /* INC_COM_COMMIDI_HPP_ */
---
+++ Core/Inc/Com/ComTxMidi.hpp
@@ -0,0 +1,19 @@
+/*
+ * ComTxMidi.hpp
+ *
+ *  Created on: Jan 23, 2022
+ *      Author: colom
+ */
+
+#ifndef INC_COM_COMTXMIDI_HPP_
+#define INC_COM_COMTXMIDI_HPP_
+
+class ComMidiTx : public ComManagerTx
+{
+
+};
+
+
+
+
+#endif /* INC_COM_COMTXMIDI_HPP_ */
