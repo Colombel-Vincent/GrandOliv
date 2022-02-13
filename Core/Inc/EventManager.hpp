64c0106ea62cf7e7b7f0f94303c3aea515bceeea
Colombel-Vincent <colombel.vincent.27@gmail.com>
2022-02-13 21:13:44 +0100

    Add Notify method, attach methode, detach method, sorting and control methode for subscriber list in eventManager

--- Core/Inc/EventManager.hpp
+++ Core/Inc/EventManager.hpp
@@ -8,17 +8,39 @@
 #ifndef INC_OBSERVER_HPP_
 #define INC_OBSERVER_HPP_
 
+#include <vector>
+
 #include <list>
 
-#include "observers_types.hpp"
-#include "MidiFixture/Button.hpp"
+#include "MidiFixture/MidiFixture.hpp"
+#include "Com/ComManager.hpp"
+
+class eventSubscriber
+{
+
+    virtual int Attach(void* ptr, user::attach_type_event evt) = 0;
+    virtual int Detach(void* ptr, user::attach_type_event evt) = 0;
+};
 
 /*This class manage all the event that comme from midi reception and update on the current event all the involved classes*/
 class EventManager : eventSubscriber
 {
 protected:
+    const uint8_t DEFAULT_VALUE_INDEX = 255;
 	
-    EventManager(uint8_t _value) : test(_value) {};
+    EventManager() 
+    {
+
+        for (int i = 0; i < 16; i++)
+            for (int g = 0; g < 127; g++)
+                MidiFixtureIndex[i][g] = DEFAULT_VALUE_INDEX;
+
+        _ComMidi =NULL;
+        _ComUART =NULL;
+        _ComI2C =NULL;
+
+    };
+
     static EventManager* _eventManager;
     uint8_t test;
 public:
@@ -29,40 +51,58 @@ public:
 
     static EventManager *GetInstance();
 
-    void update(IObserver * test) {
-        int nu = 8;
-        test->Update(&nu);
+    void Update(IObserver * test) {
+
     }
-    template<class T>
-    void notify() {};
-    template<>
-    void notify <MidiFixture>() {};
 
+    int Notify (user::update_type_event evt, void* ptr) ;
+    int Attach(void* ptr,user:: attach_type_event evt) ;
+    int Detach(void* ptr, user::attach_type_event evt) ;
 
-    //int registerEncodeur (Encodeur * en);
-    //int registerFader (Fader * fd);
-    //int registerButton(MidiFixture * md);
-    //int registerRedLedButton(ButtonRedLed * bt);
-    //int registerGreenLedButton (ButtonGreenLed * bt);
-    //int registerfaderLed (Fader * fd);
-    //int registerCom (ComManager * com);
+    void ExecuteTx();
+    void midiMsgManager(MidiMsg* msg);
     
 private :
 
-    /*List of all encodeur that have to listen event from midi Com*/
-	//std::list<Encodeur> EncodeurSubscriberList;
-	/*List of all fader that have to listen event from midi Com*/
-	//std::list<Fader> FaderSubscriberList;
+    /*Add MidiFxiture in a subscriber list*/
+    void AddMidiFixtureInSubscriber(std::vector<MidiFixture*>* p_subsriberList, MidiFixture* p_mdiFx, bool indexingMidiFixture = false);
+
+    /*Delete MidiFixture in a subscriber list*/
+    void DeleteMidiFixtureInSubscriber(std::vector<MidiFixture*>* p_subsriberList, MidiFixture* p_mdiFx, bool eraseIndexingMidiFixture = false);
+    
+    /*update all midiFixture list*/
+    int UpdateAllFixtureList();
+
+    /*Check have several same value for id*/
+    int CheckDuplicatesInMidiSubscribersList(std::vector<MidiFixture*>* p_subsriberList);
+
+    /*Sort in croissant order subsriber list*/
+    int SortMidiFixtureSubsrciberList(std::vector<MidiFixture*>* p_subsriberList);
+
+    /*Find midi Fixture in the subcribers list and return his place*/
+    int FindMidiFixtureInSubcriber(std::vector<MidiFixture*>* p_subsriberList, MidiFixture* p_mdiFx);
+
+    /*Update index for midifixture subscriber*/
+    void UpdateIndexOfMidiFixture();
 	/*List of all MidiFixture  that have to listen event from midi Com*/
-	std::list<MidiFixture> ButtonSubscriberList;
+	std::vector<MidiFixture*> MidiFixtureSubscriberList;
+
+    /*This Index Contains all adress in the array to optimize the search in the large list*/
+    uint8_t MidiFixtureIndex[16][127]; 
+
 	/*List of all button with green led that have to listen event from midi Com*/
-    std::list<ButtonGreenLed>  GreenLedSubscriberList;
+    std::vector<MidiFixture*>  GreenLedSubscriberList;
+
     /*List of all button with Red Led that have to listen event from midi Com*/
-    std::list<ButtonRedLed>  RedLedSubscriberList;
+    std::vector<MidiFixture*>  RedLedSubscriberList;
+
     /*List of all Fader with led string that have to listen event from midi Com*/
-   // std::list<faderLed>  FaderLedSubscriberList;
-    /*List of all Com "Arduino and I2C" that have to listen event from midi Com*/
-  //  std::list <ComManager> ComBufferList;
+    std::vector<MidiFixture*>  FaderLedSubscriberList;
+
+
+    ComTx * _ComUart;
+    ComTx * _ComMidi;
+    ComTx * _ComI2C;
 
 };
 
--- Core/Src/EventManager.cpp
+++ Core/Src/EventManager.cpp
@@ -7,14 +7,188 @@
 
 
 #include "../Inc/EventManager.hpp"
-
-
+#include <algorithm>
+EventManager * EventManager::_eventManager =NULL;
 
  EventManager * EventManager::GetInstance()
 {
 	if(_eventManager==nullptr){
-		_eventManager = new EventManager(5);
+		_eventManager = new EventManager;
 	    }
 	return _eventManager;
 }
 
+ /*notify manager*/
+ int EventManager::Notify(user::update_type_event evt, void* ptr)
+ {
+	 switch (evt)
+	 {
+	 case user::update_type_event::MidiMsgTx:
+		 ComSubscriberList.at(user::com::MidiTx)->Update(evt, ptr);
+	 case  user::update_type_event::NewNote:
+		 return UpdateAllFixtureList(); // Update all the list to sort in croissant order
+	 case  user::update_type_event::NewChannel:
+		 return UpdateAllFixtureList();
+	 case  user::update_type_event::MidiMsgRx:
+		 MidiFixtureSubscriberList.at(1)->Update(evt, ptr);
+	 default:
+		 break;
+	 }
+	 return 0;
+ }
+
+ void EventManager::ExecuteTx()
+ {
+	 while (!bufferTxMidi.empty())
+	 {
+		 void* msg = bufferTxMidi.front();
+		 bufferTxMidi.pop_back();
+	 }
+ }
+
+ void EventManager::midiMsgManager(MidiMsg* msg)
+ {
+
+
+ }
+
+ int EventManager::UpdateAllFixtureList()
+ {
+	 int err = 0;
+	 if (CheckDuplicatesInMidiSubscribersList(&RedLedSubscriberList))
+		 return 1;
+	 if (CheckDuplicatesInMidiSubscribersList(&GreenLedSubscriberList))
+		 return 1;
+	 if (CheckDuplicatesInMidiSubscribersList(&MidiFixtureSubscriberList))
+		 return 1;
+	 SortMidiFixtureSubsrciberList(&RedLedSubscriberList);
+	 SortMidiFixtureSubsrciberList(&GreenLedSubscriberList);
+	 SortMidiFixtureSubsrciberList(&MidiFixtureSubscriberList);
+	 UpdateIndexOfMidiFixture();
+	 return 0;
+ }
+
+
+ void EventManager::UpdateIndexOfMidiFixture()
+ {
+	 for (int i = 0; i < 16; i++)
+		 for (int g = 0; g < 127; g++)
+			 MidiFixtureIndex[i][g] = 0;
+
+	for (auto * it : MidiFixtureSubscriberList)
+	{
+		MidiFixtureIndex[it->GetChannel()][it->GetNote()] = FindMidiFixtureInSubcriber(&MidiFixtureSubscriberList, it);
+	}
+
+ }
+
+ int EventManager::CheckDuplicatesInMidiSubscribersList(std::vector<MidiFixture*>* p_subsriberList)
+ {
+	 for (auto it : *p_subsriberList)
+	 {
+		 for (auto it2 : *p_subsriberList)
+			 if (it != nullptr && it2 != nullptr)
+			 {
+				 if (it->GetId() == it2->GetId() && it != it2) // if multiple same key
+				 {
+					 return 1;
+				 }
+			 }
+	 }
+	 return 0;
+ }
+
+
+ int EventManager::Attach(void* ptr, user::attach_type_event evt)
+ {
+
+	 switch (evt)
+	 {
+	 case user::attach_type_event::ButtonGreenLed:
+		 AddMidiFixtureInSubscriber(&GreenLedSubscriberList, (MidiFixture*)ptr);
+		 AddMidiFixtureInSubscriber(&MidiFixtureSubscriberList, (MidiFixture*)ptr);
+
+	 case user::attach_type_event::ButtonRedLed:
+		 AddMidiFixtureInSubscriber(&RedLedSubscriberList, (MidiFixture*)ptr);
+		 AddMidiFixtureInSubscriber(&MidiFixtureSubscriberList, (MidiFixture*)ptr);
+
+	 case user::attach_type_event::Button:
+		 AddMidiFixtureInSubscriber(&MidiFixtureSubscriberList, (MidiFixture*)ptr);
+
+	 case user::attach_type_event::ComI2C:
+		 if(_ComI2C==NULL)
+			 return 1;
+		 _ComI2C = ptr;
+	 case user::attach_type_event::ComUART:
+		 if(_ComUart==NULL)
+			 return 1;
+		 _ComUart = ptr;
+	 case user::attach_type_event::ComMidi:
+		 if(_ComMidi==NULL)
+			 return 1;
+		 _ComMidi = ptr;
+	 }
+
+	 return 0;
+ }
+
+
+ /**/
+ int EventManager::SortMidiFixtureSubsrciberList(std::vector<MidiFixture*>* p_subsriberList)
+ {
+	 std::sort(p_subsriberList->begin(), p_subsriberList->end());
+	 return 0;
+ }
+
+ /**/
+ void EventManager::AddMidiFixtureInSubscriber(std::vector<MidiFixture*>* p_subsriberList, MidiFixture* p_mdiFx,bool indexingMidiFixture)
+ {
+	 p_subsriberList->push_back(p_mdiFx);
+	 SortMidiFixtureSubsrciberList(p_subsriberList);
+
+	 if (indexingMidiFixture)
+		 MidiFixtureIndex[p_mdiFx->GetChannel()][p_mdiFx->GetNote()] = FindMidiFixtureInSubcriber(p_subsriberList, p_mdiFx);
+ }
+
+ void EventManager::DeleteMidiFixtureInSubscriber(std::vector<MidiFixture*>* p_subsriberList, MidiFixture* p_mdiFx, bool eraseIndexingMidiFixture)
+ {
+
+	 int offset = FindMidiFixtureInSubcriber(p_subsriberList, p_mdiFx);
+	 p_subsriberList->erase(p_subsriberList->begin() + offset);
+
+	 if (eraseIndexingMidiFixture)
+		 MidiFixtureIndex[p_mdiFx->GetChannel()][p_mdiFx->GetNote()] = DEFAULT_VALUE_INDEX;
+ }
+
+ int EventManager::FindMidiFixtureInSubcriber(std::vector<MidiFixture*>* p_subsriberList, MidiFixture* p_mdiFx)
+ {
+	 int index = 0;
+	 for (auto it : *p_subsriberList)
+	 {
+		 if (it == p_mdiFx)
+			 return index;
+		 index++;
+	 }
+	 return -1;
+ }
+
+
+ int EventManager::Detach(void* ptr, user::attach_type_event evt)
+ {
+	 switch (evt)
+	 {
+	 case user::attach_type_event::ButtonGreenLed:
+		 DeleteMidiFixtureInSubscriber(&GreenLedSubscriberList, (MidiFixture*)ptr);
+	 case user::attach_type_event::ButtonRedLed:
+		 DeleteMidiFixtureInSubscriber(&RedLedSubscriberList, (MidiFixture*)ptr);
+	 case user::attach_type_event::Button:
+		 DeleteMidiFixtureInSubscriber(&MidiFixtureSubscriberList, (MidiFixture*)ptr);
+	 case user::attach_type_event::ComMidi:
+		 _ComMidi =NULL;
+	 case user::attach_type_event::ComI2C:
+		 _ComI2C =NULL;
+	 case user::attach_type_event::ComUART:
+		 _ComUart =NULL;
+	 }
+	 return 0;
+ }
